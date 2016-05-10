7731cc886f45bbd9cad1f4ab549502f574ad3411
qiaoxibj <qiaoxibj@cn.ibm.com>
2016-05-10 12:45:18 +0800

    SpringBoot security

---
+++ starter-microservice-springboot-security/src/main/webapp/sample/myProject-application/src/main/java/application/springboot/security/MvcConfig.java
@@ -0,0 +1,19 @@
+package application.springboot.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/springbootsecurity/home").setViewName("security-home");
+        registry.addViewController("/springbootsecurity").setViewName("security-home");
+        registry.addViewController("/springbootsecurity/hello").setViewName("security-hello");
+        registry.addViewController("/springbootsecurity/login").setViewName("security-login");
+        registry.addViewController("/springbootsecurity/logout").setViewName("security-home");
+    }
+
+}
