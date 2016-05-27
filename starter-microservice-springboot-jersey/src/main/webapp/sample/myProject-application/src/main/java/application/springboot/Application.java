9c5ba2b2eb72717ed265ca5a5e1b93b599ae3943
qiaoxi <qiaoxi@qiaoxi1.cn.ibm.com>
2016-05-27 10:58:24 +0800

    Optimize code to make muti srpingboot technology work together better and update  the description

--- starter-microservice-springboot-jersey/src/main/webapp/sample/myProject-application/src/main/java/application/springboot/Application.java
+++ starter-microservice-springboot-jersey/src/main/webapp/sample/myProject-application/src/main/java/application/springboot/Application.java
@@ -1,4 +1,4 @@
-package application.springboot.security;
+package application.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
