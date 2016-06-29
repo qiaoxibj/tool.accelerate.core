2c2df921320eccb354a315306f06954d16efb942
qiaoxi <qiaoxi@qiaoxi1.cn.ibm.com>
2016-06-29 16:11:23 +0800

    Update copyright info

--- starter-microservice-springboot-jersey/src/main/webapp/sample/myProject-application/src/main/java/application/springboot/jersey/Endpoint.java
+++ starter-microservice-springboot-jersey/src/main/webapp/sample/myProject-application/src/main/java/application/springboot/jersey/Endpoint.java
@@ -1,18 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2016 IBM Corp.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/ 
 
 package application.springboot.jersey;
 
@@ -24,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+//Define which URL to map to this class
 @Component
 @Path("/")
 public class Endpoint {
@@ -34,6 +20,7 @@ public class Endpoint {
 		this.objectMapper = objectMapper;
 	}
 
+	//Handling GET method, and return a welcome string
 	@GET
 	public String message() {
 		return "Hello from the SpringBoot Jersey endpoint!";
--- starter-microservice-springboot-jersey/src/main/webapp/sample/myProject-wlpcfg/src/test/java/it/springboot/jersey/SampleJerseyApplicationTests.java
+++ starter-microservice-springboot-jersey/src/main/webapp/sample/myProject-wlpcfg/src/test/java/it/springboot/jersey/SampleJerseyApplicationTests.java
@@ -1,18 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2016 IBM Corp.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/ 
 
 package it.springboot.jersey;
 
@@ -26,10 +11,11 @@ import javax.ws.rs.core.Response;
 import it.EndpointTest;
 import org.junit.Test;
 
- 
+//To test endpoint is working correctly 
 public class SampleJerseyApplicationTests extends EndpointTest {
     @Test
     public void testJerseyApplication() {
+    	//Assert the return string is as expect
         testEndpoint("/springbootjersey", "Hello from the SpringBoot Jersey endpoint!");
     }
 }
